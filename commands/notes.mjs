cd161ad974cf3737a9fbae30b43168e6aa22a7bf
Raphael <raphaelbetz@gmail.com>
2020-10-05 23:36:06 +0200

    change notes to be usable by each user individually

--- commands/notes.mjs
+++ commands/notes.mjs
@@ -12,58 +12,78 @@ export default {
     execute(message, args) {
         let file = editJsonFile(`./notes/notes.json`);
 
+        if (file.get()[message.author.id] === undefined) {
+            file.set([message.author.id], []);
+            file.save()
+        }
+
         if (args[0] === undefined) {
-            let notes = ""
-            file.get().notes?.forEach((entry, index) => {
-                notes += ` \`${(index + 1)}:\` ${entry}\n`
+            let notesString = ""
+            file.get()[message.author.id]?.forEach((noteEntry, index) => {
+                notesString += ` \`${(index + 1)}:\` ${noteEntry}\n`
             })
-            if (notes === "") notes = "Deine Notizliste ist leer."
-            message.channel.send(
-                new Discord.MessageEmbed({
-                    title: `Notizliste`,
-                    description: notes
-                })
-            )
+            if (notesString === "") notesString = "Deine Notizliste ist leer."
+
+            this.embedNotes(message, args, notesString, 0)
         } else if (args[0] === "add") {
             let notes = file.get()
-            let oneIndex = notes.notes?.push(args.shift() && args.join(' '))
-            file.set("notes", notes.notes);
+            notes[message.author.id]?.push(args.shift() && args.join(' '))
+            file.set(message.author.id, notes[message.author.id]);
             file.save();
 
             let notesString = ""
-            notes.notes?.forEach((entry, index) => {
-                notesString += ` \`${(index + 1)}:\` ${index+1===notes.notes?.length?"**":""}${entry}${index+1===notes.notes?.length?"**":""}\n`
+            notes[message.author.id]?.forEach((entry, index) => {
+                notesString += ` \`${(index + 1)}:\` ${index+1===notes[message.author.id]?.length?"**":""}${entry}${index+1===notes[message.author.id]?.length?"**":""}\n`
             })
-
-            message.channel.send(
-                new Discord.MessageEmbed({
-                    title: `Notizliste`,
-                    description: `Hier ist deine neue Notizliste:\n${notesString}`,
-                    color: 0x4CAF50
-                })
-            )
+            this.embedNotes(message, args, notesString, 1)
         } else if (args[0] === "remove") {
             let notes = file.get()
-            let oldNote = JSON.stringify(notes.notes)
-            let removed = notes.notes?.splice((parseInt(args[1]) - 1), 1)
-            file.set("notes", notes.notes);
+            let indexToRemove = parseInt(args[1], 10)
+            if(isNaN(indexToRemove)) {
+                this.embedErrorInvalidArguments(message, args)
+                return
+            }
+            if(notes[message.author.id].length < indexToRemove) {
+                this.embedErrorInvalidRemoveIndex(message, args)
+                return
+            }
+
+            let oldNotes = JSON.stringify(notes[message.author.id])
+            notes[message.author.id]?.splice((parseInt(args[1]) - 1), 1)
+            file.set(message.author.id, notes[message.author.id]);
             file.save();
 
             let notesString = ""
-            JSON.parse(oldNote)?.forEach((entry, index) => {
+            JSON.parse(oldNotes).forEach((entry, index) => {
                 notesString += ` \`${(index + 1)}:\` ${index+1===(parseInt(args[1]))?"**~~":""}${entry}${index+1===(parseInt(args[1]))?"~~**":""}\n`
             })
             if (notesString === "") notesString = "Deine Notizliste ist leer."
 
-            message.channel.send(
-                new Discord.MessageEmbed({
-                    title: `Notizliste`,
-                    description: `Hier ist deine neue Notizliste:\n${notesString}`,
-                    color: 0xff6f00
-                })
-            )
+            this.embedNotes(message, args, notesString, 2)
+
         } else {
-            yadBot.sendCommandErrorEmbed(message, `Incorrect arguments were given.\n Use \`${config.prefix}help\` to get more information`)
+            this.embedErrorInvalidArguments(message, args)
         }
+    },
+    embedNotes(message, args, notesString, embedType) {
+        let finalEmbedColor
+        if (embedType === 0) { finalEmbedColor = null }             // no color
+        else if (embedType === 1) { finalEmbedColor = 0x4CAF50 }    // added note (green)
+        else if (embedType === 2) { finalEmbedColor = 0xff6f00 }    // removed note (red)
+        else { finalEmbedColor = embedType }                        // custom color
+
+        message.channel.send(
+            new Discord.MessageEmbed({
+                title: `Notizliste`,
+                description: embedType === 0 ? notesString : `Hier ist deine neue Notizliste:\n${notesString}`,
+                color: finalEmbedColor
+            })
+        )
+    },
+    embedErrorInvalidArguments(message, args) {
+        yadBot.sendCommandErrorEmbed(message, `Invalid arguments were given.\n Use \`${config.prefix}help\` for more information`)
+    },
+    embedErrorInvalidRemoveIndex(message, args) {
+        yadBot.sendCommandErrorEmbed(message, `Invalid index to remove was given.`)
     }
 }
