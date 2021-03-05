cf06aab53d99a4eb09440f1b7b5adc938634c892
Raphael <raphaelbetz@gmail.com>
2021-03-05 21:12:24 +0100

    rename ScraperInterfaceInGame.mjs to ScraperInterfaceInGameGames.mjs, add ScraperInterfaceInGameArticles.mjs

--- classes/ScraperInterfaceInGameArticles.mjs
+++ classes/ScraperInterfaceInGameArticles.mjs
@@ -1,17 +1,15 @@
 import jsdom from 'jsdom'
 import * as Discord from 'discord.js'
 import { WebsiteScraper } from './WebsiteScraper'
-import config from '../config.json'
-import luxon from 'luxon'
 
-class ScraperInterfaceInGame extends WebsiteScraper {
+class ScraperInterfaceInGameArticles extends WebsiteScraper {
 
     constructor() {
         super()
     }
 
     getScrapingUrl() {
-        return 'https://interfaceingame.com/games/?sortby=recent_add'
+        return 'https://interfaceingame.com/articles/?sortby=recent_add'
     }
 
     getScrapingInterval() {
@@ -30,7 +28,7 @@ class ScraperInterfaceInGame extends WebsiteScraper {
 
             entry.image = element.querySelector('div.item__media > picture > img')?.getAttribute('data-src')
             entry.title = element.querySelector('span.item__title-text')?.textContent.trim()
-            entry.date = luxon.DateTime.fromFormat(element.querySelector('h3.item__subtitle')?.textContent.trim(), 'MMMM d, yyyy').setZone('Europe/Berlin').toISO()
+            entry.subtitle = element.querySelector('p.item__excerpt').textContent.trim()
             entry.url = element.querySelector('a.item__title-link')?.getAttribute('href')
             entry.genres = []
             element.querySelectorAll('ul > li.tags__item:not(.tags__item--dots)').forEach((genre, genreIndex) => {
@@ -44,8 +42,7 @@ class ScraperInterfaceInGame extends WebsiteScraper {
     }
 
     generateFileNameFromJson(json) {
-        let dateString = luxon.DateTime.fromISO(json.date).toFormat('yyyy-MM-dd')
-        let fileName = `${dateString}-${json.title.substring(0, 50)}`
+        let fileName = `${json.title.substring(0, 50)}`
         return this.generateSlugFromString(fileName) + '.json'
     }
 
@@ -54,25 +51,20 @@ class ScraperInterfaceInGame extends WebsiteScraper {
 
         return new Discord.MessageEmbed(
             {
-                'title': 'A new game interface has been added!',
-                'description': `${json.title}\'s interface has been added.`,
+                'title': `New Article: ${json.title}`,
+                'description': json.subtitle,
                 'url': `https://interfaceingame.com/${json.url}`,
                 'image': {
                     'url': json.image,
                 },
                 'author': {
                     'name': 'Interface In Game',
-                    'url': 'https://interfaceingame.com/games/?sortby=recent_add',
+                    'url': 'https://interfaceingame.com/articles/?sortby=recent_add',
                     'icon_url': 'https://etzbetz.io/stuff/yad/images/logo_interface_in_game.png',
                 },
                 'fields': [
                     {
-                        'name': 'Release date',
-                        'value': luxon.DateTime.fromISO(json.date).toFormat('MMMM d, yyyy'),
-                        inline: true
-                    },
-                    {
-                        'name': 'Genre(s)',
+                        'name': 'Category(s)',
                         'value': this.getGenreString(json.genres),
                         inline: true
                     },
@@ -96,4 +88,4 @@ class ScraperInterfaceInGame extends WebsiteScraper {
     }
 }
 
-export default new ScraperInterfaceInGame()
+export default new ScraperInterfaceInGameArticles()
--- classes/YadBot.mjs
+++ classes/YadBot.mjs
@@ -6,7 +6,8 @@ import scraperGuildWars2News from './ScraperGuildWars2News.mjs'
 import scraperTeamspeakBadges from './ScraperTeamspeakBadges.mjs'
 import ScraperMovieReleases from './ScraperMovieReleases.mjs'
 import ScraperXRelReleases from './ScraperXRelReleases.mjs'
-import ScraperInterfaceInGame from './ScraperInterfaceInGame.mjs'
+import ScraperInterfaceInGameGames from './ScraperInterfaceInGameGames.mjs'
+import ScraperInterfaceInGameArticles from './ScraperInterfaceInGameArticles.mjs'
 import { log, debugLog } from '../index'
 import files from './Files.mjs'
 import activityTypes from '../constants/ActivityTypes.mjs'
@@ -47,7 +48,8 @@ class YadBot {
                 scraperTeamspeakBadges,
                 ScraperMovieReleases,
                 ScraperXRelReleases,
-                ScraperInterfaceInGame,
+                ScraperInterfaceInGameGames,
+                ScraperInterfaceInGameArticles
             ]
         })
 
