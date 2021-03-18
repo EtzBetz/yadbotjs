eb38be741ce7e2224c52e8e92d03656192efeab6
Raphael <raphaelbetz@gmail.com>
2021-03-18 17:32:49 +0100

    improve ScraperFreeSteamGames.mjs, change edit date for release date, show discounted price, add logging

--- classes/ScraperFreeSteamGames.mjs
+++ classes/ScraperFreeSteamGames.mjs
@@ -1,7 +1,8 @@
 import luxon from 'luxon'
 import * as Discord from 'discord.js'
-import { WebsiteScraper } from './WebsiteScraper'
+import {WebsiteScraper} from './WebsiteScraper'
 import files from './Files.mjs'
+import yadBot from './YadBot.mjs';
 
 class ScraperFreeSteamGames extends WebsiteScraper {
 
@@ -36,7 +37,6 @@ class ScraperFreeSteamGames extends WebsiteScraper {
 
                     // call super method here because method in this class also refreshes last scraping timestamp
                     let detailResponse = await super.requestWebsite(`https://store.steampowered.com/api/appdetails/?appids=${game.appid}&cc=de&l=german`)
-                    /*console.log(detailResponse.data)*/
                     if (detailResponse.data[game.appid.toString()].success === true) {
                         const detailData = detailResponse.data[game.appid.toString()].data
                         if (
@@ -57,10 +57,10 @@ class ScraperFreeSteamGames extends WebsiteScraper {
 
                             entry.id = game.appid
                             entry.title = detailData.name
-                            entry.imageUrl = detailData.header_image
+                            entry.imageUrl = detailData.header_image.substring(0, detailData.header_image.indexOf("?") !== -1 ? detailData.header_image.indexOf("?") : detailData.header_image.length)
                             entry.developers = detailData.developers
                             entry.publishers = detailData.publishers
-                            entry.date = game.last_modified
+                            entry.date = this.getReleaseDate(detailData.release_date?.date)
 
                             const originalPrice = detailData.price_overview?.initial?.toString().padStart(3, '0')
                             if (originalPrice !== undefined) {
@@ -69,11 +69,19 @@ class ScraperFreeSteamGames extends WebsiteScraper {
                                 const priceDecimal = originalPrice.substring(originalPrice.length - 2)
                                 entry.originalPrice = `${priceEuro},${priceDecimal}€`
                             }
-                            console.log(entry)
+
+                            const finalPrice = detailData.price_overview?.final?.toString().padStart(3, '0')
+                            if (finalPrice !== undefined) {
+                                const decimalPosition = finalPrice.length - 2
+                                const priceEuro = finalPrice.substring(0, decimalPosition)
+                                const priceDecimal = finalPrice.substring(finalPrice.length - 2)
+                                entry.finalPrice = `${priceEuro},${priceDecimal}€`
+                            }
+                            // console.log(entry)
                             elements.push(entry)
                         }
                     } else {
-                        console.log('detailed request was unsuccessful')
+                        yadBot.sendMessageToOwner(`Detailed request was unsuccessful for ID ${game.appid}`)
                     }
                 }
             }
@@ -83,7 +91,7 @@ class ScraperFreeSteamGames extends WebsiteScraper {
     }
 
     generateFileNameFromJson(json) {
-        let dateString = luxon.DateTime.fromSeconds(json.date).toFormat('yyyy-MM-dd')
+        let dateString = luxon.DateTime.fromISO(json.date).toFormat('yyyy-MM-dd')
         let fileName = `${dateString}-${this.generateSlugFromString(json.title)}`
         return this.generateSlugFromString(fileName) + '.json'
     }
@@ -94,15 +102,15 @@ class ScraperFreeSteamGames extends WebsiteScraper {
         let descriptionText = ''
 
         switch (content.discountType) {
-        case 'discounted':
-            descriptionText = `Um ${content.discountAmount}% reduziert.`
-            break
-        case 'free':
-            descriptionText = `Free to play im Steam Store.`
-            break
-        case 'gift':
-            descriptionText = `Aktuell kostenlos im Steam Store.`
-            break
+            case 'discounted':
+                descriptionText = `Um ${content.discountAmount}% reduziert.`
+                break
+            case 'free':
+                descriptionText = `Free to play im Steam Store.`
+                break
+            case 'gift':
+                descriptionText = `Aktuell kostenlos im Steam Store.`
+                break
         }
 
         let embed = new Discord.MessageEmbed(
@@ -119,11 +127,31 @@ class ScraperFreeSteamGames extends WebsiteScraper {
             },
         )
 
+        if (content.finalPrice !== undefined) {
+            embed.fields.push(
+                {
+                    'name': 'Rabattpreis',
+                    'value': `**${content.finalPrice}**`,
+                    'inline': true,
+                },
+            )
+        }
+
         if (content.originalPrice !== undefined) {
             embed.fields.push(
                 {
                     'name': 'Originalpreis',
                     'value': `~~${content.originalPrice}~~`,
+                    'inline': true,
+                },
+            )
+        }
+
+        if (content.date !== undefined) {
+            embed.fields.push(
+                {
+                    'name': 'Erscheinungsdatum',
+                    'value': luxon.DateTime.fromISO(content.date).toFormat('d. LLLL yyyy', { locale: "de" }),
                     'inline': true,
                 },
             )
@@ -139,6 +167,41 @@ class ScraperFreeSteamGames extends WebsiteScraper {
 
     getSortingFunction() {
         return this.sortJsonByIsoDateAndTitleProperty
+    }
+
+    getReleaseDate(dateString) {
+        let steamDateRegexType1 = /(\d+). ([a-zA-ZöÖäÄüÜ]{3,}).? (\d{4})/
+        let steamDateRegexType2 = /(\d{2})\/(\d{2})\/(\d{4})/
+        let monthAliases = ["Jan", "Feb", "März", "Apr", "Mai", "Juni", "Juli", "Aug", "Sep", "Okt", "Nov", "Dez"]
+
+        let dateRegexResult = steamDateRegexType1.exec(dateString)
+        let day, month, year
+        if (dateRegexResult !== null) {
+            day = dateRegexResult[1]
+            month = dateRegexResult[2]
+            year = dateRegexResult[3]
+
+            let monthNumber = monthAliases.findIndex((monthAlias) => {
+                return monthAlias === month
+            })
+            if (monthNumber === -1) {
+                yadBot.sendMessageToOwner(`Date '${dateString}' with month '${month}' failed in getReleaseDate() [2]`)
+            }
+            day = day.padStart(2, '0')
+            month = (monthNumber+1).toString().padStart(2, '0')
+        } else {
+            dateRegexResult = steamDateRegexType2.exec(dateString)
+            day = dateRegexResult[1]
+            month = dateRegexResult[2]
+            year = dateRegexResult[3]
+
+            if (dateRegexResult === null) {
+                yadBot.sendMessageToOwner(`Date '${dateString}' failed in getReleaseDate() [1]`)
+                return
+            }
+        }
+        // console.log(luxon.DateTime.fromFormat(`${day}.${month}.${year}`, 'd.M.yyyy').setZone('Europe/Berlin').toISO())
+        return luxon.DateTime.fromFormat(`${day}.${month}.${year}`, 'd.M.yyyy').setZone('Europe/Berlin').toISO()
     }
 }
 
